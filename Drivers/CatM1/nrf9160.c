afcaa78224f96a543512cbb40ee13c84cc5f6900
phb1018 <33706406+parkhongbeom@users.noreply.github.com>
2024-10-07 12:19:14 +0900

    nrf9160 catm1 add

---
+++ Drivers/CatM1/nrf9160.c
@@ -0,0 +1,270 @@
+/*
+ * nrf9160.c
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: phb10
+ */
+
+#include <nrf9160.h>
+#include "stm32u5xx_hal.h"
+#include <main.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define MINMEA_MAX_SENTENCE_LENGTH 256
+
+uint8_t uart_cat_m1_buf[MINMEA_MAX_SENTENCE_LENGTH] = {0, };
+uint8_t cat_m1_parse_cnt = 0;
+uint8_t cat_m1_parse_ret = 0;
+
+uart_cat_m1_t uart_cat_m1_rx;
+cat_m1_at_cmd_rst_t cat_m1_at_cmd_rst_rx;
+
+extern uint8_t wpmInitFlag;
+bool nrf9160_checked = false;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART1)
+    {
+    	push(&uart_cat_m1_rx, uart_cat_m1_rx.temp);
+    	HAL_UART_Receive_IT(&huart1, &uart_cat_m1_rx.temp, 1);
+    }
+}
+
+void clear_uart_buf(uart_cat_m1_t* u)
+{
+  u->head = 0;
+  u->tail = 0;
+  memset(u->buffer, 0, sizeof(u->buffer));
+}
+
+void push(uart_cat_m1_t* u, uint8_t data)
+{
+  u->buffer[u->head] = data;
+
+  u->head++;
+
+  if (u->head >= CAT_M1_UART_BUF_SIZE)
+  {
+    u->head = 0;
+  }
+}
+
+uint8_t pop(uart_cat_m1_t* u)
+{
+  uint8_t data = u->buffer[u->tail];
+
+  u->tail++;
+
+  if (u->tail >= CAT_M1_UART_BUF_SIZE)
+  {
+    u->tail = 0;
+  }
+
+  return data;
+}
+
+uint8_t isEmpty(uart_cat_m1_t* u)
+{
+  return u->head == u->tail;
+}
+
+
+bool send_at_command(const char *cmd)
+{
+	HAL_UART_Transmit_IT(&huart1, (uint8_t*)cmd, strlen(cmd));
+	PRINT_INFO("TX CMD >>> %s\r\n",cmd);
+	//return true;
+	receive_at_command_ret();
+}
+
+bool receive_at_command_ret()
+{
+//	if (strstr(&uart_cat_m1_rx, str) != NULL)
+//    {
+//        //PRINT_INFO("RX Data >>> %s\r\n", uart_cat_m1_buf);
+//        return true;
+//    }
+	if(cat_m1_parse_ret)
+	{
+		return true;
+	}
+    return false;
+}
+
+bool receive_response(void)
+{
+    // 버퍼가 비어 있지 않으면 데이터를 처리
+    if (isEmpty(&uart_cat_m1_rx) == 0) {
+        uart_cat_m1_rx.rxd = pop(&uart_cat_m1_rx);
+
+        // '\r' 또는 '\n'이 수신되면 현재까지의 버퍼 내용을 처리
+        if (uart_cat_m1_rx.rxd == '\r' || uart_cat_m1_rx.rxd == '\n') {
+            // '\r' 또는 '\n'도 버퍼에 추가
+            uart_cat_m1_buf[cat_m1_parse_cnt] = uart_cat_m1_rx.rxd;
+            cat_m1_parse_cnt++;
+
+            // 수신한 데이터를 출력 및 파싱
+            //PRINT_INFO("RX Data >>> %s\r\n", uart_cat_m1_buf);
+            cat_m1_parse_process(uart_cat_m1_buf);
+
+            // 버퍼 초기화 및 카운터 리셋
+            memset(&uart_cat_m1_buf, 0, MINMEA_MAX_SENTENCE_LENGTH);
+            cat_m1_parse_cnt = 0;
+        } else {
+            // '\r' 또는 '\n'이 아닌 문자들은 버퍼에 저장
+            if (cat_m1_parse_cnt < MINMEA_MAX_SENTENCE_LENGTH - 1) {  // 버퍼 크기 제한 체크
+                uart_cat_m1_buf[cat_m1_parse_cnt] = uart_cat_m1_rx.rxd;
+                cat_m1_parse_cnt++;
+            }
+        }
+    }
+    return true;
+}
+
+bool cat_m1_parse_process(uint8_t *msg)
+{
+    char command[MAX_CMD_LEN] = {0};
+    char value[MAX_VALUE_LEN] = {0};
+
+    char *colon_pos = strchr((char *)msg, ':');
+
+    if (colon_pos != NULL)
+    {
+        int cmd_len = colon_pos - (char *)msg;
+        strncpy(command, (char *)msg, cmd_len);
+        command[cmd_len] = '\0';
+
+        colon_pos++;
+        int i = 0;
+
+        while (i < MAX_VALUE_LEN - 1 && strncmp(colon_pos, "OK", 2) != 0 && strncmp(colon_pos, "ERROR", 5) != 0 && strncmp(colon_pos, "Ready", 5) != 0)
+        {
+            if (*colon_pos == '\0' || *colon_pos == '\r' || *colon_pos == '\n')
+            {
+                break;
+            }
+            value[i++] = *colon_pos++;
+        }
+        value[i] = '\0';
+        //printf("AT Command: %s\r\n", command);
+        //printf("Value: %s\r\n", value);
+
+        cat_m1_parse_result((char *)command, (char *)value);
+
+    }
+    else
+    {
+    	if (strstr((char *)msg, "Ready"))
+		{
+			printf("Response: Ready\r\n");
+			cat_m1_parse_ret = 1;
+			return true;
+		}
+    	else if (strstr((char *)msg, "OK"))
+        {
+            printf("Response: OK\r\n");
+            cat_m1_parse_ret = 1;
+            return true;
+        }
+        else if (strstr((char *)msg, "ERROR"))
+        {
+            printf("Response: ERROR\r\n");
+            cat_m1_parse_ret = 0;
+            return false;
+        }
+    }
+
+    return false;
+}
+
+void cat_m1_parse_result(const char *command, const char *value)
+{
+    printf("Command: %s\r\n", command);
+    printf("Value: %s\r\n", value);
+
+    if (strstr(command, "+COPS") != NULL)
+    {
+        printf("1>>>\r\n");
+        strcpy(cat_m1_at_cmd_rst_rx.cops, (const char *)value);
+        PRINT_INFO("COPS result >>> %s\r\n", cat_m1_at_cmd_rst_rx.cops);
+
+    }
+    if (strstr(command, "+CGDCONT") != NULL)
+    {
+        printf("2>>>\r\n");
+        strcpy(cat_m1_at_cmd_rst_rx.cgdcont, (const char *)value);
+        PRINT_INFO("CGDCONT result >>> %s\r\n", cat_m1_at_cmd_rst_rx.cgdcont);
+    }
+    if (strstr(command, "%XICCID") != NULL)
+    {
+        printf("3>>>\r\n");
+        strcpy(cat_m1_at_cmd_rst_rx.iccid, (const char *)value);
+        PRINT_INFO("ICCID result >>> %s\r\n", cat_m1_at_cmd_rst_rx.iccid);
+    }
+
+
+}
+
+// Initialize UART
+void uart_init()
+{
+	HAL_UART_Receive_IT(&huart1, &uart_cat_m1_rx.temp, 1);
+	clear_uart_buf(&uart_cat_m1_rx);
+	clear_uart_buf(&cat_m1_at_cmd_rst_rx);
+}
+
+void nrf9160_init()
+{
+    uart_init();
+    PRINT_INFO("nRF9160 initialized\r\n");
+}
+
+void nrf9160_ready(void)
+{
+	while (!wpmInitFlag)
+	{
+		//cat_m1_send("AT\r\n");
+		//cat_m1_recv("OK");
+		if (receive_at_command_ret())
+		{
+			PRINT_INFO("Cat.M1 already available\r\n");
+			wpmInitFlag = 1;
+		}
+		else if (send_at_command("AT\r\n"))
+		{
+			PRINT_INFO("Cat.M1 already available\r\n");
+			wpmInitFlag = 1;
+		}
+		HAL_Delay(500);
+	}
+}
+
+void nrf9160_check()
+{
+	if (send_at_command("AT+CFUN=1\r\n"))
+	{
+		//return true;
+	}
+
+	if (send_at_command("AT+COPS?\r\n"))
+	{
+		//return true;
+	}
+	if (send_at_command("AT+CGDCONT?\r\n"))
+	{
+		//return true;
+	}
+	if (send_at_command("AT%XICCID\r\n"))
+	{
+		//return true;
+	}
+	nrf9160_checked = true;
+
+
+//	PRINT_INFO("COPS result >>> %s\r\n", cat_m1_at_cmd_rst_rx.cops);
+//	PRINT_INFO("CGDCONT result >>> %s\r\n", cat_m1_at_cmd_rst_rx.cgdcont);
+//	PRINT_INFO("ICCID result >>> %s\r\n", cat_m1_at_cmd_rst_rx.iccid);
+}
---
+++ Drivers/CatM1/nrf9160.h
@@ -0,0 +1,64 @@
+/*
+ * nrf9160.h
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: phb10
+ */
+
+#ifndef CATM1_NRF9160_H_
+#define CATM1_NRF9160_H_
+
+#include "stm32u5xx_hal.h"
+#include <stdbool.h>
+
+// Constants
+#define MAX_CMD_LEN 100
+#define MAX_VALUE_LEN 100
+#define MAX_PARAMS_LEN 100
+#define MAX_RESPONSE_LEN 256
+
+#define ICCID_LEN 20
+
+#define CAT_M1_UART_BUF_SIZE 512
+
+typedef struct {
+  uint8_t head;
+  uint8_t tail;
+  uint8_t buffer[CAT_M1_UART_BUF_SIZE];
+  uint8_t rxd;
+  uint8_t temp;
+  bool done;
+} uart_cat_m1_t;
+
+typedef struct {
+  uint8_t buffer[MAX_RESPONSE_LEN];
+  uint8_t cops[MAX_VALUE_LEN];
+  uint8_t cgdcont[MAX_VALUE_LEN];
+  uint8_t iccid[ICCID_LEN];
+} cat_m1_at_cmd_rst_t;
+
+extern uart_cat_m1_t uart_cat_m1_rx;
+
+void clear_uart_buf(uart_cat_m1_t* u);
+void push(uart_cat_m1_t* u, uint8_t data);
+uint8_t pop(uart_cat_m1_t* u);
+uint8_t isEmpty(uart_cat_m1_t* u);
+
+// External UART handle (replace huart1 with the appropriate UART handle if needed)
+extern UART_HandleTypeDef huart1;
+
+// UART Communication related functions
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+bool send_at_command(const char *cmd);
+bool receive_at_command_ret(void);
+bool receive_response(void);
+bool cat_m1_parse_process(uint8_t *msg);
+void cat_m1_parse_result(const char *command, const char *value);
+
+// nRF9160 Modem Interaction
+void uart_init(void);
+void nrf9160_init(void);
+void nrf9160_ready(void);
+void nrf9160_check(void);
+
+#endif /* CATM1_NRF9160_H_ */
