0095daa40772d4574d0ec92bbe4853308b8e6cea
phb1018 <33706406+parkhongbeom@users.noreply.github.com>
2024-10-29 21:39:32 +0900

    Recommit

--- Core/Src/app_freertos.cpp
+++ Core/Src/app_freertos.cpp
@@ -134,7 +134,7 @@ float deltaAlt = 0;
 uint16_t curr_height = 0;
 int height_num = 0;
 
-float falling_threshold = 0.90; // 낙상 판별 기준 높이 차이
+float falling_threshold = 1.30; // 낙상 판별 기준 높이 차이
 
 uint8_t ssSCD = 0;
 uint16_t ssHr = 0;
