7437661bcc963fb58f87f67c10f0c49e81f8eff0
phb1018 <33706406+parkhongbeom@users.noreply.github.com>
2025-01-20 15:50:47 +0900

    GPS buf fix

--- Drivers/CatM1/nrf9160.c
+++ Drivers/CatM1/nrf9160.c
@@ -421,20 +421,26 @@ void handle_gps_command(const char *value)
     }
 
     else { // strstr(value, "1,4") => after MSG: GPS DATA
-       	if (gpsDataLength > 10) {
-       	    char tempBuffer[sizeof(cat_m1_at_cmd_rst.gps)];
-       	    int j = 0;
+    	if (gpsDataLength > 10) {
+    	    char tempBuffer[sizeof(cat_m1_at_cmd_rst.gps)];
+    	    int j = 0;
+    	    int commaCount = 0;
 
-       	    for (int i = 0; value[i] != '\0' && j < sizeof(tempBuffer) - 1; i++) {
-       	        if (value[i] != '"') {
-       	            tempBuffer[j++] = value[i];
-       	        }
-       	    }
-       	    tempBuffer[j] = '\0';
+    	    for (int i = 0; value[i] != '\0' && j < sizeof(tempBuffer) - 1; i++) {
+    	        if (value[i] == ',') {
+    	            commaCount++;
+    	        }
+    	        if (value[i] != '"') {
+    	            tempBuffer[j++] = value[i];
+    	        }
+    	    }
+    	    tempBuffer[j] = '\0';
 
-       	    strncpy((char *)cat_m1_at_cmd_rst.gps, tempBuffer, sizeof(cat_m1_at_cmd_rst.gps) - 1);
-       	    cat_m1_at_cmd_rst.gps[sizeof(cat_m1_at_cmd_rst.gps) - 1] = '\0';
-       	}
+    	    if (commaCount == 6) {
+    	        strncpy((char *)cat_m1_at_cmd_rst.gps, tempBuffer, sizeof(cat_m1_at_cmd_rst.gps) - 1);
+    	        cat_m1_at_cmd_rst.gps[sizeof(cat_m1_at_cmd_rst.gps) - 1] = '\0';
+    	    }
+    	}
     }
 }
 
